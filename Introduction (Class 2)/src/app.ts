3dd0a0da00d1abc74758917c58aace5e4d319b4b
christopher <a01225198@itesm.mx>
2017-08-07 20:05:12 -0500

    First class template

---
+++ Introduction (Class 2)/src/app.ts
@@ -0,0 +1,10 @@
+
+// This is a comment, I am just text that means nothing to the code, but information to the developer
+
+console.log("Hello World"); // Writing our first code line, let's greet the world
+
+console.log(2); // console.log(something) writes whatever we input it to the console
+
+console.log(true); // To write something to the console, write it between parenthesis
+
+console.log("You may have seen that all these lines finish with a semicolon"); // These are called statements
