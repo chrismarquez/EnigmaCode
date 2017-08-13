3fadab5c58f12f9b461a56796743c1bf6dd351f5
christopher <a01225198@itesm.mx>
2017-08-12 22:12:09 -0500

    First class

--- Introduction (Class 2)/src/app.ts
+++ Introduction (Class 2)/src/app.ts
@@ -8,3 +8,29 @@ console.log(2); // console.log(something) writes whatever we input it to the con
 console.log(true); // To write something to the console, write it between parenthesis
 
 console.log("You may have seen that all these lines finish with a semicolon"); // These are called statements
+
+let word = "This is a string, saved in a variable" // Variables are reusable memory storage
+
+let digit = 5; // Variables can hold different data types
+
+word = "Another string" // We can reuse variables like this
+
+let str: string; // We can also declare the variable, define it later
+str = "I am the Senate" // Now we assign the variable, we can only use the string type we declared
+ // Trying to assign other types to str will cause a compile error
+
+let x: number = 24; // Let's declare and define two numbers
+let y: number = 5;
+
+console.log(x + y); // Add
+console.log(x * y); // Multiply
+console.log(x - y); // Substract
+console.log(x / y); // Divide (Real Division)
+console.log(Math.floor(x / y)); // Divide (Whole Division)
+console.log(Math.ceil(x / y)); // We can round up numbers too
+console.log(x % y); // Remainder operation (The remainder of the whole division)
+
+let z: number = 10;
+
+console.log(x * 2 / z + y); // Respect the order of operations from math
+
