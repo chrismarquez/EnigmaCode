50b3a913ef24d07bc335b621e7f1408352788846
christopher <a01225198@itesm.mx>
2017-08-27 21:20:54 -0500

    Class 3 Complete

---
+++ (Class 3) Control Structures: Conditional/src/app.ts
@@ -0,0 +1,44 @@
+let a; // Let declares a new variable
+let b: string = "s"; // string indicates the text type
+let c: number; // Number indicates numeric types, both real and whole;
+function d() {} // The function keyword declares a new function, equivalent to let for variables
+if (2 > 3) {} // The if declares a conditional expression
+else {} // The else keyword defines what happens when if evaluates false
+for (;false;) {} // For decleares a sequential cycle
+while(false) {} // While declares a conditional cycle
+function f() {return ""} // Return is a special keyword for functions
+switch(""){} // Switch is used for multivalue selection
+switch(b){case "s": break;} // Case indicates one of the values handled by switch, break stops execution
+try {} catch(e){} finally {} // Try and catch are used for exception handling
+const g = 3; // Const is used to declare constant variables
+class X {} // Declares a new class
+export default class Y {} // export and default are used to handle modules
+async function Z() {await ""} // async/await are used to handle asynchronous operations
+
+let temp: number = 24;
+
+if (temp > 24) {
+    console.log("A good temp");
+} else {
+    console.log("A little cold");
+}
+
+let x: number = 0;
+
+if (x > 0) { 
+    console.log(x + "is a positive number");
+} else if (x < 0) {
+    console.log(x + "is a negative number");
+} else {
+    console.log("zero");
+}
+
+let line: string = "Do it";
+
+if (line == "Do it") {
+    console.log("I am the senate");
+} else {
+    console.log("You underestimate my power");
+}
+
+
